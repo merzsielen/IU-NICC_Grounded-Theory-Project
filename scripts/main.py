6460ce4ed74aca451034a0a3ceb04d6b9c3b1a0a
Collin Brown <CJBrown7966@gmail.com>
2024-12-01 21:28:46 -0500

    Implemented Knowledge Graph Extraction
    Right now, this uses OpenAI models to extract knowledge graphs. Ideally, we want to make this work with locally hosted models.

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,24 @@
 # Settings                                                                                        #
 ###################################################################################################
 
+import neo4j
 import os.path
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.core import PropertyGraphIndex
+from llama_index.core import SimpleDirectoryReader
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.graph_stores.neo4j import Neo4jPropertyGraphStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.indices.property_graph import SimpleLLMPathExtractor
+
+using_openai = True
+
+llm_path = os.path.dirname(__file__) + "/../models/Llama-3.2-1B"
+embed_path = os.path.dirname(__file__) + "/../models/bert-base-uncased"
+
+openai_key_path = os.path.dirname(__file__) + "/../util/openai_key"
 
 neo4j_uri = "bolt://localhost:7687"
 neo4j_username = "neo4j"
@@ -14,8 +31,6 @@ neo4j_password_path = os.path.dirname(__file__) + "/../util/password"
 # First, we're going to connect to the local neo4j database. To do this, we'll have to store some
 # information in a file in /util.
 
-import neo4j
-
 def GetTxt(path):
 	if os.path.isfile(path):
 		return open(path, "r").read()
@@ -23,6 +38,12 @@ def GetTxt(path):
 
 def ConnectDriver(uri, auth):
 	return neo4j.GraphDatabase.driver(uri, auth)
+
+###################################################################################################
+# Extraction                                                                                      #
+###################################################################################################
+
+# ...
 
 ###################################################################################################
 # Main                                                                                            #
@@ -34,24 +55,55 @@ def main():
 	if (neo4j_password == ""):
 		print("Password file not found.")
 		return
-	credentials = (neo4j_username, neo4j_password)
+
+	# Now, if we're using OpenAI, we load the key and set up our
+	# LLM and Embedding Model.
+	if using_openai:
+		openai_key = GetTxt(openai_key_path)
+		if (openai_key == ""):
+			print("OpenAI API Key not found.")
+			return
+		os.environ["OPENAI_API_KEY"] = openai_key
+
+		llm = llm=OpenAI(model="gpt-3.5-turbo", temperature=0.0)
+		embed_model = OpenAIEmbedding(model_name="text-embedding-3-small")
+	else:
+		llm = HuggingFaceLLM(model_name=llm_path)
+		embed_model = HuggingFaceEmbedding(model_name=embed_path)
+
+	Settings.llm = llm
+
+	# Next, we feed in our documents,
+	documents = SimpleDirectoryReader(os.path.dirname(__file__) + "/../data/").load_data()
+
+	# connect to the graph database,
+	graph_store = Neo4jPropertyGraphStore(
+		username=neo4j_username,
+		password=neo4j_password,
+		url=neo4j_uri,
+	)
 	
-	# Now, we connect to the Neo4j database.
-	with neo4j.GraphDatabase.driver(neo4j_uri, auth=credentials) as driver:
-		try:
-			driver.verify_connectivity()
-		except:
-			print("Unable to the driver. Check that you are using the correct password and that the database is active.")
-		print("Successfully connected to the database.")
+	# and then parse each document into the database.
+	index = PropertyGraphIndex.from_documents(
+		documents,
+		embed_model=embed_model,
+		kg_extractors = [SimpleLLMPathExtractor(
+    		llm=llm,
+    		max_paths_per_chunk=10,
+    		num_workers=4)],
+		property_graph_store=graph_store,
+		show_progress=True
+		)
 
-		with driver.session(database="neo4j") as session:
+	# This is just an example query.
+	retriever = index.as_retriever(include_text=False)
+	results = retriever.retrieve("What happened in Bloomington in 2024")
+	for record in results:
+		print(record.text)
 
-			# Here is where we would run our code, extracting units of meaning and connecting them
-			# in the database.
-
-			session.close()
-
-		driver.close
-
+	# query_engine = index.as_query_engine(include_text=True)
+	# response = query_engine.query("What happened in Bloomington in 2024?")
+	# print(str(response))
+	
 if __name__ == '__main__':
     main()
